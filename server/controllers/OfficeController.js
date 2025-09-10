3d6485abeaee899189eabbda851b1302b5bda4e7
Rick Mastry <jahrik@hotmail.com>
2025-09-10 13:56:07 -0400

    added console log for auth to debug blank notification

--- server/controllers/OfficeController.js
+++ server/controllers/OfficeController.js
@@ -3,8 +3,9 @@ import User from "../models/User.js";
 
 export const registerOffice = async (req, res) => {
     try {
+        console.log("registerOffice called, req.auth:", req.auth);
         const {name, address, contact, city} = req.body;
-        const owner = req.auth.userId;
+        const owner = req.auth?.userId;
 
          if (!owner) {
           return res.status(401).json({ success: false, message: "Not authenticated" });
