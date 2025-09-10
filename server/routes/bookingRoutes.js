70a1c20cb4bf511ad50c66508a08b3970124e2f5
Rick Mastry <jahrik@hotmail.com>
2025-09-10 15:56:46 -0400

    moved to requireAuth on all routes

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,12 +1,13 @@
 import express from'express'
 import { checkAvailabilityAPI, createBooking, getOfficeBookings, getUserBookings } from '../controllers/bookingController.js'
 import { protect } from '../middleware/authMiddleWare.js';
+import { requireAuth } from "@clerk/express";
 
 const bookingRouter = express.Router()
 
 bookingRouter.post('/check-availability', checkAvailabilityAPI)
-bookingRouter.post('/book', protect, createBooking);
-bookingRouter.get('/user', protect, getUserBookings);
-bookingRouter.get('/office', protect, getOfficeBookings);
+bookingRouter.post('/book', requireAuth(), createBooking);
+bookingRouter.get('/user', requireAuth(), getUserBookings);
+bookingRouter.get('/office', requireAuth(), getOfficeBookings);
 
 export default bookingRouter
--- server/routes/workspaceRoute.js
+++ server/routes/workspaceRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
+import { requireAuth } from "@clerk/express";
 import { protect } from "../middleware/authMiddleWare.js";
 import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from "../controllers/workController.js";
 
@@ -8,7 +9,7 @@ const workspaceRouter = express.Router();
 workspaceRouter.post('/', upload.array("images", 4),createRoom);
 workspaceRouter.get('/', getRooms);
 workspaceRouter.get('/owner', protect, getOwnerRooms);
-workspaceRouter.post('/toggle-availability', protect, toggleRoomAvailability);
+workspaceRouter.post('/toggle-availability', requireAuth(), toggleRoomAvailability);
 
 
 export default workspaceRouter;
