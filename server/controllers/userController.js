d90f5671ecfad8f9ece5b400d4ed92dd2507d332
Rick Mastry <jahrik@hotmail.com>
2025-09-10 13:17:20 -0400

    added requireauth to user route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,13 +2,24 @@
 
 export const getUserData = async (req, res) => {
     try {
-        const role = req.user.role;
+        const clerkUserId = req.auth.userId;
+        // Lookup or create user in MongoDB
+    let user = await User.findById(clerkUserId);
+    if (!user) {
+      user = await User.create({ _id: clerkUserId, role: "user" });
+    }
+        //const role = req.user.role;
         const recentSearchedCities = req.user.recentSearchedCities;
-        res.json({success: true, role, recentSearchedCities})
+         res.json({ success: true, role: user.role, recentSearchedCities: user.recentSearchedCities });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+      /*  res.json({success: true, role, recentSearchedCities})
     } catch (error) {
         res.json({success: false, message: error.message})
     }
-}
+}*/
 
 
 //store users searched offices
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getUserData, storeRecentSearchedCities } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleWare.js';
+import { requireAuth } from "@clerk/express";
 
 const UserRouter = express.Router();
 
-UserRouter.get('/', protect, getUserData);
+UserRouter.get('/', requireAuth(), getUserData);
 UserRouter.post('/store-recent-search', protect, storeRecentSearchedCities);
 
 export default UserRouter;
